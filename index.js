aecc66031efb8e22ab9d401adafe9652c0c97423
shmibblez <shmibblez@gmail.com>
2020-08-14 13:40:47 -0500

    fixed key catastrophe

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
+    return Promise.resolve()
 })
 client.on("message", msg => {
     if (msg.content.includes("ping")) {
         msg.reply("Pong!")
     }
+    return Promise.resolve();
 })
 client.login(process.env.login_key);
