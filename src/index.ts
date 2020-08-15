e736bd3a30b022188e18b578ec42a6340e214e8b
shmibblez <shmibblez@gmail.com>
2020-08-15 17:51:23 -0500

    now using typescript

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+// const Discord = require('discord.js')
+import * as Discord from 'discord.js'
+const client = new Discord.Client()
+client.on("ready", () => {
+    // console.log(`Logged in as ${client.user.tag}!`)
+    return Promise.resolve()
+})
+client.on("message", msg => {
+    if (!msg.content.startsWith('!dankbot')) return;
+    const commands = msg.content.split(' ');
+    switch (commands[1]) {
+        default:
+        case 'help':
+            msg.reply('whats good homie' +
+                '\n\ncommands are seperated by spaces, for example: \n`!dankbot fry brutal` deepfries the latest picture in the chat brutally' +
+                '\nahere are some commands:' +
+                '\n  - `deepfry`' +
+                '\n    - `mild`' +
+                '\n    - `medium` (default)' +
+                '\n    - `brutal`' +
+                '\n - `help`')
+            break;
+        case 'fry':
+            msg.reply('this feature is coming sooon')
+            break;
+    }
+    return Promise.resolve();
+})
+
+function deepFry() {
+
+}
+client.login(process.env.login_key);
