a23512660b8aa58bdbf4ca2bbfba0a778f1f5253
shmibblez <shmibblez@gmail.com>
2020-08-15 19:25:54 -0500

    added fry feature

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 // const Discord = require('discord.js')
-import * as Discord from 'discord.js'
+import Discord from 'discord.js'
+import { inspect } from 'util'
+import Jimp from 'jimp'
+import { randomBytes } from 'crypto'
 const client = new Discord.Client()
 client.on("ready", () => {
     // console.log(`Logged in as ${client.user.tag}!`)
     return Promise.resolve()
 })
-client.on("message", msg => {
+client.on("message", async msg => {
     if (!msg.content.startsWith('!dankbot')) return;
     const commands = msg.content.split(' ');
     switch (commands[1]) {
@@ -21,13 +24,51 @@ client.on("message", msg => {
                 '\n - `help`')
             break;
         case 'fry':
-            msg.reply('this feature is coming soooooon')
+            // msg.reply('this feature is coming soooooon')
+            deepFry(msg)
             break;
     }
     return Promise.resolve();
 })
 
-function deepFry() {
+async function deepFry(msg: Discord.Message) {
+    const filter = (m: Discord.Message) => m.attachments.size > 0;
+    const messages = await msg.channel.awaitMessages(filter, { max: 1 })
+    console.log('got messages:\n' + inspect(messages))
+    let url!: string;
+    for (const m of messages) {
+        url = m[1].url
+        if (url) break;
+    }
+    if (!url) {
+        msg.reply('no image found')
+        return;
+    }
+    console.log('url: ' + url)
+
+    const imgPath = __dirname + '/toasty' + randomBytes(10).toString('hex') + '.png';
+    console.log('new image path: ' + imgPath)
+    const img = await Jimp.read(url, (e, jimage) => {
+        if (e) {
+            msg.reply('something failed homie')
+            return;
+        }
+        jimage
+            .pixelate(40)
+            .contrast(0.95)
+            .posterize(1)
+            .write(imgPath)
+    })
+    if (img) {
+        msg.channel.send({
+            files: [{
+                attachment: imgPath,
+                name: 'epic'
+            }]
+        })
+        return;
+    }
+
 
 }
 client.login(process.env.login_key);
