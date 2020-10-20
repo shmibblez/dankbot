9e91f8356dab1fa6b5364d76ff3058f15b454e53
shmibblez <shmibblez@gmail.com>
2020-10-20 15:43:14 -0500

    added fry feature

--- src/index.ts
+++ src/index.ts
@@ -136,5 +136,4 @@ function deepfryGif({ url, filePath, msg }: { url: string, filePath: string, msg
 
 
 
-client.login(process.env.login_key);
-console.log('----------------------------login key: ' + process.env.login_key)
+client.login(process.env.dankbot_login_key);
